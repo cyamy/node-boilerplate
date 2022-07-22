20372e45e52535e82a3698e1c7d9ece29caaa360
cyamy <hi@cyamy.dev>
2022-07-23 00:19:14 +0900

    feat: support in-source testing

---
+++ src/inline.ts
@@ -0,0 +1,40 @@
+const sub =
+  (x: number) =>
+  (y: number): number =>
+    x - y;
+
+const div =
+  (x: number) =>
+  (y: number): number =>
+    x / y;
+
+const goodbye = () => {
+  return {
+    baz: 'goodbye',
+    qux: 'world',
+  };
+};
+
+if (import.meta.vitest) {
+  const { assert, describe, it, expect } = import.meta.vitest;
+
+  describe('calc', () => {
+    it('sub', () => {
+      expect(sub(2)(1)).eq(1);
+    });
+
+    it('div', () => {
+      expect(div(2)(1)).eq(2);
+    });
+
+    it('goodbye', () => {
+      const expectResult = {
+        baz: 'goodbye',
+        qux: 'world',
+      };
+      const result = goodbye();
+
+      assert.deepEqual(expectResult, result);
+    });
+  });
+}
