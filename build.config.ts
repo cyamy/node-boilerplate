c0227f2b493822fd4494623c4b06ef14cb0d82d8
cyamy <hi@cyamy.dev>
2022-07-23 19:21:42 +0900

    feat: add bundler

---
+++ build.config.ts
@@ -0,0 +1,9 @@
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+  entries: ['src/index'],
+  outDir: 'build',
+  replace: {
+    'import.meta.vitest': 'undefined',
+  },
+});
---
+++ build/index.mjs
@@ -0,0 +1,8 @@
+const add = (x) => (y) => x + y;
+const mul = (x) => (y) => x * y;
+const hello = () => ({
+  foo: 'hello',
+  bar: 'world',
+});
+
+export { add, hello, mul };
